f2615e9b796e5c695826ea6bece03ce8457a78b2
JohnsirK <1241010946@qq.com>
2020-09-14 17:41:04 +0800

    base

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import Vue from 'vue';
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import VueRouter from 'vue-router';
 Vue.config.productionTip = false;
 
-new Vue({
+Vue.use(Element)
+
+//跳转相同路由报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location: import("vue-router").RawLocation) {
+	//@ts-ignore
+	return originalPush.call(this, location).catch((err: any) => err)
+}
+
+router.afterEach((to) => {
+	if (to.meta.title) {
+		document.title = to.meta.title
+	}
+	window.scrollTo(0, 0)
+})
+
+export const app = new Vue({
   router,
   store,
   render: (h) => h(App),

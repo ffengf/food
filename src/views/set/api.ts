e575ee57917e7583caabfd5f2436dea0404d053a
myf <532864961@qq.com>
2020-10-10 09:31:36 +0800

    set

--- src/views/set/api.ts
+++ src/views/set/api.ts
@@ -3,21 +3,24 @@
 
 import { Http_list, Http } from '@/http';
 import { has_id, list } from '@/http/base';
-import { Id, parmas } from "@/types/global"
+import { Id } from "@/types/global"
 
 export interface set_info {
 	contract:string
+	title:string
+	id:Id
+	store_type:number
 }
 
 
 class HttpSet extends Http {
-	get_info():Promise<set_info>{
+	get_info():Promise<set_info[]>{
 		return this.get()
 	}
 
-	save(info:set_info){
-		return this.server.put(`${this.uri}`,info)
+	save({ contract,id,store_type }:set_info){
+		return this.server.put(`${this.uri}${id}/`,{ contract,store_type })
 	}
 }
 
-export const api_set = new HttpSet('/food_admin/setting/1/')
+export const api_set = new HttpSet('/food_admin/setting/')
--- src/views/set/default/index.vue
+++ src/views/set/default/index.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
-		<div class="box bbb" style="margin-bottom: 15px;">
-            <h1>合同申明:</h1>
+		<div class="box bbb" style="margin-bottom: 15px;" v-for="ele in info" :key="ele.id">
+            <h1>{{ ele.title }}</h1>
 			<br>
-			<Editor v-model="info.contract" />
-			<el-button class="btn" type="primary" @click="save">保存</el-button>
+			<Editor v-model="ele.contract" />
+			<el-button class="btn" type="primary" @click="save(ele)">保存</el-button>
         </div>
 	</div>
 </template>
@@ -12,20 +12,19 @@
 <script lang='ts'>
 import Editor from "@/components/editor/index.vue";
 import { Vue, Component } from "vue-property-decorator";
-import { api_set } from "@/views/set/api"
+import { api_set,set_info } from "@/views/set/api"
+import { Id } from '@/types/global';
 @Component({
 	components:{
 		Editor
 	}
 })
 export default class extends Vue {
-	info = {
-		contract:""
-	}
+	info:set_info[] = []
 
 
-	async save(){
-		await api_set.save(this.info)
+	async save(val:set_info){
+		await api_set.save(val)
 		this.$message.success('保存成功')
 		this.get_info()
 	}
